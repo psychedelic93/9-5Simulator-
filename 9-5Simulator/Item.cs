dd93b39c0b5974a4be2ba7aa41bc4b24b9d63427
Alexey YUGAI <alexey.yugai@ginini-antipode.com>
2020-01-13 15:09:55 +0100

    commit

---
+++ 9-5Simulator/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_5Simulator
+{
+    class Item
+    {
+        public string Name { get; set; }
+
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ 9-5Simulator/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_5Simulator
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Depression { get; set; }
+        public int Money { get; set; }
+        public List<Item> Inventory { get; set; }
+
+        public Player(string name, int health, int depression, int money)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Depression = depression;
+            this.Money = money;
+            this.Inventory = new List<Item>();
+        }
+    }
+}
